dd9c9cc925cdfadc2faaf7f26baa377c014e5919
Grace Adaeze Okengwu <136967329+docgany@users.noreply.github.com>
2025-11-13 20:07:23 +0000

    Building a simple counter

---
+++ simple-counter/script.js
@@ -0,0 +1,9 @@
+/*Project 1: Build a simple counter.
+
+    Requirements:
+-Create a Github repository for this project. 
+- The count starts at 0
+    - Have two buttons on the screen, one to increase(+) the count by 1, and one button to lower(-) the count by 1.
+        - Pressing the plus button increases the count by 1, pressing the minus button decreases by 1
+            - Create a Github repository for this project and write Git commits as you go.
+- Send me the project for review and get started on the next project.*/
